7dce61c8cc1f7a21b4efd3b92970b819cb80b05f
VanKhaos <39248582+VanKhaos@users.noreply.github.com>
2020-06-17 00:32:24 +0200

    init

--- src/de/thunderfrog/Main.java
+++ src/de/thunderfrog/Main.java
@@ -1,8 +1,10 @@
 package de.thunderfrog;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,53 +12,60 @@ public class Main {
     public static String Path, Filename,action;
     public static Systemlog Console = new Systemlog();
     public static Scanner in = new Scanner(System.in);
+    public static ArrayList<String> Folders = new ArrayList<String>();
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         Console.Log("Welcome to dir2list" + version);
         InputPath();
     }
 
-    public static void InputPath(){
+    public static void InputPath() throws IOException {
         // Path
         Console.Log("Please enter the full Path:");
         Path = in.nextLine();
 
         // Filename
         Console.Log("Please enter the Filename:");
-        Filename = in.nextLine() + ".txt";
-        CreateFile();
+        Filename = "Output/" + in.nextLine() + ".txt";
+        dir2file();
 
         Console.Log("(E)xit or (R)estart ? ");
         action = in.nextLine();
         CheckAction();
 
     }
-    // Create File with Input
-    public static void CreateFile(){
+    public static void dir2file() {
         try {
-            File myFile = new File("Output/" + Filename);
 
-            if (myFile.createNewFile()) {
-                Console.Log("File created: " + myFile.getName());
-            } else {
-                Console.Log("File already exists.");
+            FileWriter FolderWriter = new FileWriter(Filename);
+            // File Header
+            FolderWriter.write("dir2list Folders\n\n");
+
+            // Folders to Arraylist
+            Files.list(new File(Path).toPath())
+                    .forEach(path -> {
+                        Folders.add(path.toString());
+                    });
+
+            // Folder Array to String
+            // Write Array to File
+            for (String folder : Folders) {
+                FolderWriter.write(folder + "\n");
             }
+
+            // FileWrite Close
+            FolderWriter.close();
+
+            Console.Log("Successfully wrote to the file. \n" +
+                            "Filename: " + Filename + " | Elements: " + Folders.size());
         } catch (IOException e) {
             Console.Log("An error occurred.");
             e.printStackTrace();
         }
     }
 
-
-    public static void dir2file(String Path, String Filename) throws IOException {
-        Files.list(new File(Path).toPath())
-                .forEach(path -> {
-                    Console.Log(path.toString());
-                });
-    }
-
     // Check Input End or Restart
-    public static void CheckAction(){
+    public static void CheckAction() throws IOException {
         if(action.equals("E") || action.equals("e")){
             Console.Log("Exit Program");
             System.exit(0);
